27aaab6a9401d49537b0bfcb374799cbf4387797
dunncw <dunncw@g.cofc.edu>
2024-10-08 19:04:45 -0400

    fixed but where empty values in excel where input into form as 'nan'. now it will leave it empty

--- src/web_automation/excel_processor.py
+++ src/web_automation/excel_processor.py
@@ -13,18 +13,18 @@ def extract_data_from_excel(excel_path):
     for _, row in df.iterrows():
         person = {
             "individual_name": {
-                "first": row['First 1'],
-                "middle": row['Middle 1'],
-                "last": row['Last 1']
+                "first": row['First 1'] if pd.notnull(row['First 1']) else "",
+                "middle": row['Middle 1'] if pd.notnull(row['Middle 1']) else "",
+                "last": row['Last 1'] if pd.notnull(row['Last 1']) else ""
             },
             "additional_name": {
-                "first": row['First 2'],
-                "middle": row['Middle 2'],
-                "last": row['Last 2']
+                "first": row['First 2'] if pd.notnull(row['First 2']) else "",
+                "middle": row['Middle 2'] if pd.notnull(row['Middle 2']) else "",
+                "last": row['Last 2'] if pd.notnull(row['Last 2']) else ""
             },
-            "date_on_deed": row['date on deed'].strftime('%m/%d/%Y') if pd.notnull(row['date on deed']) else None,
-            "sales_price": f"{row['Sales Price']:.2f}" if pd.notnull(row['Sales Price']) else None,
-            "contract_number": row['Contract Num']
+            "date_on_deed": row['date on deed'].strftime('%m/%d/%Y') if pd.notnull(row['date on deed']) else "",
+            "sales_price": f"{row['Sales Price']:.2f}" if pd.notnull(row['Sales Price']) else "",
+            "contract_number": str(row['Contract Num']) if pd.notnull(row['Contract Num']) else ""
         }
         people_data.append(person)
     
